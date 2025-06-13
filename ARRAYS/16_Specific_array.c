f4a0111fe3e4c10791a265ba77357ee9d5120e0d
Ridham1409 <ridhambhavnagariya@gmail.com>
2025-06-13 19:58:42 +0530

    Array 15,16 uploaded

---
+++ ARRAYS/16_Specific_array.c
@@ -0,0 +1,32 @@
+// Search for a specific number in an array of 10 integers and print "Found" or "Not Found".
+#include <stdio.h>
+int main()
+{
+    int arr[10];
+    int i,num;
+    printf("Enter 10 Arrays:\n\n");
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("Array[%d]: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter a number to search: ");
+    scanf("%d", &num);
+
+    for (i = 0; i < 10; i++)
+    {
+        if (num == arr[i])
+        {
+            printf("Founded %d \n", num);
+        }
+        else
+        {
+            printf("Not Found %d \n", num);
+        }
+
+        return 0;
+
+    }
+}
---
+++ ARRAYS/17_Find_replace.c
@@ -0,0 +1,1 @@
+// Find the position (index) of a given element in an array.
