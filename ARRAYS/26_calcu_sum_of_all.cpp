050f03cda0ec3c431d851304b5da1b7b567732cc
ShaileshVSavani <svs.rnw23@gmail.com>
2025-06-16 13:50:33 +0530

    Array Codes Updated

---
+++ ARRAYS/26_calcu_sum_of_all.cpp
@@ -0,0 +1,9 @@
+// Calculate the sum of all elements in an integer array of size 5.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5}
+    int sum = 0;
+    for (int i = 0; i 
+}
