63b7952a5fbb2d85a093c98eec9be79cbd0e9a4d
ShaileshVSavani <svs.rnw23@gmail.com>
2025-06-27 12:39:23 +0530

    C++ Exception Added

--- C - String Arrays/2_read_single_word.c
+++ C - String Arrays/2_read_single_word.c
@@ -4,7 +4,7 @@ int main()
 {
     char name[50]; // reserve space for up to 49 chars + '\0'
     printf("Enter your name: ");
-    scanf("%49s", name); // read a word (stops at whitespace)
+    scanf("%4s", name); // read a word (stops at whitespace)
 
     printf("You entered: %s\n", name);
     return 0;
